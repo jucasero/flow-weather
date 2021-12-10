7ac35fda1e1a575c455956153bbe69e59f4796ff
jucasero <44075402+jucasero@users.noreply.github.com>
2021-12-10 13:23:58 -0300

    Updates

--- src/components/common/index.ts
+++
@@ -1,4 +0,0 @@
-export { Card } from './Card';
-export { Message } from './Message';
-export { Select } from './Select';
-export { Spinner } from './Spinner';
--- src/state/store.ts
+++
@@ -1,9 +0,0 @@
-import { createStore, applyMiddleware } from 'redux';
-import { composeWithDevTools } from 'redux-devtools-extension';
-import reduxThunk from 'redux-thunk';
-
-import reducers from './reducers';
-
-export type RootState = ReturnType<typeof reducers>;
-
-export const store = createStore(reducers, composeWithDevTools(applyMiddleware(reduxThunk)));
