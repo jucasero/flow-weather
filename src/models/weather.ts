336ac37b921bc8aeedd6041726d4a8ff8e204731
julian sepulveda <juliank1337@gmail.com>
2025-12-17 18:46:10 +0100

    feat: update weather model and API fetch to include daily temperature range and remove unused fields

--- src/models/weather.ts
+++ src/models/weather.ts
@@ -6,64 +6,38 @@ export interface Weather {
 	timezone: string;
 	timezone_abbreviation: string;
 	elevation: number;
-	current_units: CurrentUnits;
-	current: Current;
 	daily_units: DailyUnits;
 	daily: Daily;
 }
 
-export interface Current {
-	time: string;
-	interval: number;
-	temperature_2m: number;
-	precipitation: number;
-	apparent_temperature: number;
-	cloud_cover: number;
-	relative_humidity_2m: number;
-	rain: number;
-	surface_pressure: number;
-	wind_speed_10m: number;
-}
-
-export interface CurrentUnits {
-	time: string;
-	interval: string;
-	temperature_2m: string;
-	precipitation: string;
-	apparent_temperature: string;
-	cloud_cover: string;
-	relative_humidity_2m: string;
-	rain: string;
-	surface_pressure: string;
-	wind_speed_10m: string;
-}
-
 export interface Daily {
 	time: Date[];
+	temperature_2m_min: number[];
+	temperature_2m_max: number[];
+	apparent_temperature_mean: number[];
+	cloud_cover_mean: number[];
+	dew_point_2m_mean: number[];
+	precipitation_probability_mean: number[];
+	surface_pressure_mean: number[];
+	visibility_mean: number[];
+	wind_speed_10m_mean: number[];
 	sunrise: string[];
 	sunset: string[];
 	uv_index_max: number[];
-	rain_sum: number[];
-	precipitation_probability_max: number[];
-	relative_humidity_2m_mean: number[];
-	cloud_cover_mean: number[];
-	wind_speed_10m_mean: number[];
-	surface_pressure_mean: number[];
-	dew_point_2m_mean: number[];
-	visibility_mean: number[];
 }
 
 export interface DailyUnits {
 	time: string;
+	temperature_2m_min: string;
+	temperature_2m_max: string;
+	apparent_temperature_mean: string;
+	cloud_cover_mean: string;
+	dew_point_2m_mean: string;
+	precipitation_probability_mean: string;
+	surface_pressure_mean: string;
+	visibility_mean: string;
+	wind_speed_10m_mean: string;
 	sunrise: string;
 	sunset: string;
 	uv_index_max: string;
-	rain_sum: string;
-	precipitation_probability_max: string;
-	relative_humidity_2m_mean: string;
-	cloud_cover_mean: string;
-	wind_speed_10m_mean: string;
-	surface_pressure_mean: string;
-	dew_point_2m_mean: string;
-	visibility_mean: string;
 }
--- src/services/weather.ts
+++ src/services/weather.ts
@@ -6,7 +6,7 @@ export const getWeather = async (
 ): Promise<Weather | null> => {
 	try {
 		const response = await fetch(
-			`https://api.open-meteo.com/v1/forecast?latitude=${cityCoordinates.lat}&longitude=${cityCoordinates.lon}&daily=sunrise,sunset,uv_index_max,rain_sum,precipitation_probability_max,relative_humidity_2m_mean,cloud_cover_mean,wind_speed_10m_mean,surface_pressure_mean,dew_point_2m_mean,visibility_mean&current=temperature_2m,precipitation,apparent_temperature,cloud_cover,relative_humidity_2m,rain,surface_pressure,wind_speed_10m`,
+			`https://api.open-meteo.com/v1/forecast?latitude=${cityCoordinates.lat}&longitude=${cityCoordinates.lon}&daily=temperature_2m_min,temperature_2m_max,apparent_temperature_mean,cloud_cover_mean,dew_point_2m_mean,precipitation_probability_mean,surface_pressure_mean,visibility_mean,wind_speed_10m_mean,sunrise,sunset,uv_index_max`,
 		);
 		const data = (await response.json()) as Weather;
 		return data;
